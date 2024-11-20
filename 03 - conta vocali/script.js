333ab35109dda25f982362686702d024fef745f7
SamueleTerraroli <terraroli99@gmail.com>
2024-11-20 15:17:15 +0100

    fix: exercise 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,25 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
+const vocals = ['a','e','i','o','u']
 
 // Dichiara la funzione qui.
+const getAeiou = (parola, aeiou) => {
+    const vocal = [];
+    for (let i = 0; i < parola.length; i++) {
+        if (aeiou.includes(parola[i])) {
+            vocal.push(parola[i]);
+        }
+    }
+    return vocal;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(getAeiou(word, vocals));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+
